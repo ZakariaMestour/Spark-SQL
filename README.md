dbce2b19004c0a776d997dba3442ba185d78af40
Zakaria <zakaria.mestour-etu@etu.univh2c.ma>
2025-10-22 11:36:28 +0100

    Afficher le nombre d’incidents par service

---
+++ README.md
@@ -0,0 +1,4 @@
+Partie 1 :
+1- Afficher le nombre d’incidents par service.
+![img.png](img.png)
+2-Afficher les deux années où il a y avait plus d’incidents.
--- src/main/java/poo/Main.java
+++ src/main/java/poo/Main.java
@@ -8,12 +8,21 @@ import org.apache.spark.sql.SparkSession;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        SparkSession ss= SparkSession.builder()
-                .appName("Spark SQL")
+        SparkSession spark= SparkSession.builder()
+                .appName("Incident SQL App")
                 .master("local[*]")
+                .config("spark.ui.port", "4080")
+                .config("spark.port.maxRetries", "100")
                 .getOrCreate();
-        Dataset<Row> df1= ss.read().csv("students.csv");
-        df1.printSchema();
-
+        Dataset<Row> incidentDF= spark.read()
+                .option("header","true")
+                .option("inferSchema","true")
+                .csv("incidents.csv");
+        incidentDF.createOrReplaceTempView("incident");
+        Dataset<Row> result = spark.sql("select service,count(*) from incident group by service");
+        result.show();
+        spark.stop();
+        // 1. Afficher le nombre d’incidents par service.
+        //2. Afficher les deux années où il a y avait plus d’incidents.
     }
 }
